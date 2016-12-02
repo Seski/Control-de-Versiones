1d11d403a134e30567e117ffc0f312e19c3a6cea
alexb <alexb@DESKTOP-SO4FSCQ>
2016-12-02 20:21:30 +0100

    Creo el proyecto inicialmente

---
+++ Practica1/src/practica1/Practica1.java
@@ -0,0 +1,17 @@
+
+package practica1;
+
+/**
+ * @author Alejandro Bajo Perez
+ */
+public class Practica1 {
+
+   
+    public static void main(String[] args) {
+   
+        System.out.println("Hola !!!");
+        System.out.println(" prueba");
+        
+    }
+    
+}
