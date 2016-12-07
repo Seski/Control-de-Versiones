921a21da9b41e3f6ad9c070353d9879df72b34f5
IsmA <IsmA@deLimaMcfly>
2016-12-07 20:30:29 +0100

    Subo una clase de Ismael. Dia despues examen Julio.

--- Practica1/src/practica1/Ismael.java
+++ Practica1/src/practica1/Ismael.java
@@ -5,77 +5,64 @@
  */
 package practica1;
 
-import java.util.Scanner;
-
 /**
  *
  * @author IsmA
  */
 public class Ismael {
-    public static int pedirLongitud(){
-    
-        int longitud = 0;
-        
-        Scanner entrada = new Scanner(System.in);
-        
-        do {            
-            
-            System.out.print("Introduce una longitud: ");
-            
-            try {
-                
-                longitud = entrada.nextInt();
-                
-            } catch (Exception e) {
-                System.out.println(e);
-                System.out.println("Por favor, introduce números enteros.");
-            }
-            
-            if((longitud > 10) || (longitud < 1)){
-            
-                System.out.println("La longitud debe ser de 1 a 10.");
-            }
-            
-            
-        } while ((longitud > 10) || (longitud < 1));
-        
-    
-        
-        return longitud;
+
+    public static int aleatorio() {
+
+        double decimal;
+        int entero;
+
+        decimal = (Math.random() * 100) + 100;
+        decimal = Math.floor(decimal);
+        entero = (int) decimal;
+
+        return entero;
     }
 
-    
-    public static void rellenar(int vector[]){
-        
-     double numeroPre;
-     int numero;
-     
-        for (int i = 0; i < vector.length ; i++) {
-            
-            numeroPre = (Math.random() * 6) + 1;
-            numeroPre = Math.floor(numeroPre);
-            numero = (int) numeroPre;
-            vector[i] = numero;
-        }   
-    }
-    
-    public static void mostrar(int vector[]){
-    
-        for (int i = 0; i < vector.length; i++) {
-            
-            System.out.println("La posición " + i + " corresponde con el valor " + vector[i]);
-            
-            
+    public static void rellenar(int vector[][]) {
+
+        for (int i = 0; i < 4; i++) {
+
+            for (int j = 0; j < 3; j++) {
+                vector[i][j] = aleatorio();
+
+            }
         }
-    
+
     }
+
+    public static void mostrar(int vector[][]) {
+
+        int numero;
+
+        for (int i = 0; i < 4; i++) {
+
+            for (int j = 0; j < 3; j++) {
+
+                numero = vector[i][j];
+
+                if (numero % 2 == 0) {
+                    System.out.println("La posición " + i + j + " tendrá el valor " + vector[i][j]);
+                }
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
-        
-        System.out.println("¿Rellenar un array de números aleatorios?");
-        
-        int [] vector = new int [pedirLongitud()];        
+
+        System.out.println("Rellenar con aleatorios un Array 4x3 e imprimir los pares. \n");
+
+        int[][] vector = new int[4][3];
         rellenar(vector);
         mostrar(vector);
-        
     }
+
 }
+
+
+
